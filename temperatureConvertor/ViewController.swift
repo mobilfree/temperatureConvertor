dfbc0a7ed9e5347fa7344b332039b8c894d01e9a
Artem Shchegolev <mobilfree@mail.ru>
2021-10-11 13:03:40 +0300

    setup logic

--- temperatureConvertor/ViewController.swift
+++ temperatureConvertor/ViewController.swift
@@ -9,7 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-   
-
+    @IBOutlet weak var celsiuslabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
+    
+    @IBAction func sliderChange(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(sender.value))
+        celsiuslabel.text = "\(temperatureCelsius)ºC"
+        let fahrenheitTemperature = Int(round((sender.value * 9 / 5) + 32 ))
+        fahrenheitLabel.text = "\(fahrenheitTemperature)ºF"
+    
+    }
+    
+    
+    
 }
 
